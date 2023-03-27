a28950d6aedcd0fdab89a14ba91096c85030705d
Kittipong2001 <128574773+Kittipong2001@users.noreply.github.com>
2023-03-28 02:59:30 +0700

    Add a playRound function

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
 function getComputerChoice(){
     // Randomly return a string
-    const CHOICES = ["Rock", "Paper", "Scissors"]
+    const CHOICES = ["Rock", "Paper", "Scissors"];
     return CHOICES[Math.floor(Math.random() * CHOICES.length)];
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "Tie!";
+    }
+    else {
+        if (playerSelection == "Rock") {
+            if (computerSelection == "Scissors") {
+                return "Win!";
+            }
+            else {
+                return "Lose!";
+            }
+        }
+
+        if (playerSelection == "Paper") {
+            if (computerSelection == "Rock") {
+                return "Win!";
+            }
+            else {
+                return "Lose!";
+            }
+        }
+
+        if (playerSelection == "Scissors") {
+            if (computerSelection == "Paper") {
+                return "Win!";
+            }
+            else {
+                return "Lose!";
+            }
+        }
+    }
+  }
+
+
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
