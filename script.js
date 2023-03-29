dc3cc1daa0e21034a7734f07882f9489a901f1f7
Kittipong2001 <128574773+Kittipong2001@users.noreply.github.com>
2023-03-29 15:34:05 +0700

    Add interface

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function getComputerChoice(){
+function getComputerChoice() {
     // Randomly return a string
     const CHOICES = ["Rock", "Paper", "Scissors"];
     return CHOICES[Math.floor(Math.random() * CHOICES.length)];
@@ -36,35 +36,72 @@ function playRound(playerSelection, computerSelection) {
             }
         }
     }
-  }
+}
 
-function game() {
-    let playerSelection;
-    let numberOfWins = 0;
-    let conclusion;
-    let computerSelection;
-    for (i = 1; i <= 5; i++) {
-        playerSelection = prompt("Input");
-        computerSelection = getComputerChoice();
-        conclusion = playRound(playerSelection, computerSelection);
-        
-        if (conclusion == "Tie!") {
-            i--;
-            console.log("It's tie!");
-            continue;
-        }
-
-        if (conclusion == "Win!") {
-            console.log("You win!");
-            numberOfWins++;
-        }
-        else {
-            console.log("You lose!");
-        }
+function displayWinCondition() {
+    if (yourScore == 5) {
+        alert("YOU WIN!");
         
     }
 
-    console.log("You win " + numberOfWins + " time(s), you lose " + (5 - numberOfWins) + " time(s).");
+    if (computerScore == 5) {
+        alert("YOU LOSE!");
+    }
+        
 }
 
-game();
+function updateText() {
+    // if your score and ai's score are greater or equal to 5, make the text green.
+    scores[0].textContent = yourScore.toString();
+    scores[1].textContent = computerScore.toString();
+    
+    if (yourScore >= 5) {
+        scores[0].classList.add("green-text");
+    }
+
+    if (computerScore >= 5) {
+        scores[1].classList.add("green-text");
+    }
+
+    
+}
+const buttons = document.querySelectorAll('.buttons');
+const scores = document.querySelectorAll('.scoreboard h1');
+
+let playerSelection;
+let yourScore = 0;
+let computerScore = 0;
+
+buttons.forEach((button) => {
+    // Add a listener to each button then get the id of the clicked button
+
+    button.addEventListener('click', (e) => {
+        playerSelection = e.target.id;
+        computerSelection = getComputerChoice();
+        conclusion = playRound(playerSelection, computerSelection);
+        // Increment the win numbers
+
+        if (conclusion == "Tie!") {
+            alert("You tie this round.")
+            return;
+        }
+
+        if (conclusion == "Win!") {
+            alert("You win this round.");
+            yourScore++;
+        }
+        else {
+            alert("You lose this round.");
+            computerScore++;
+        }
+
+        displayWinCondition();
+        
+        updateText();
+        
+        
+
+    })
+});
+
+
